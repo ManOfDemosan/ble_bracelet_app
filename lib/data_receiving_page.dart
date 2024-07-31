c81b0c16f46c70239f07e4754a83665ec80444a3
ManOfDaemosan <hwanee0000@gmail.com>
2024-07-31 13:07:25 +0200

    [Feat] error message

--- lib/data_receiving_page.dart
+++ lib/data_receiving_page.dart
@@ -9,6 +9,8 @@ class DataReceivingPage extends StatefulWidget {
 
 class _DataReceivingPageState extends State<DataReceivingPage> {
   List<String> fileList = [];
+  bool isLoading = true;
+  String? errorMessage;
 
   @override
   void initState() {
@@ -16,23 +18,34 @@ class _DataReceivingPageState extends State<DataReceivingPage> {
     fetchFileListFromSDCard();
   }
 
-  void fetchFileListFromSDCard() async {
+  Future<void> fetchFileListFromSDCard() async {
     try {
       String serviceUuid = '27655500-9de4-4e89-9229-3b654a53a965';
       String characteristicUuid = '27655700-9de4-4e89-9229-3b654a53a965';
 
       print('Attempting to read SD card file list...');
       List<int> sdCardData = await CustomBluetoothService.instance.readSDCardFileList(serviceUuid, characteristicUuid);
+
+      if (sdCardData.isEmpty) {
+        throw Exception('No data received from the device.');
+      }
+
       String stringValue = String.fromCharCodes(sdCardData);
       print('Received data: $stringValue');
 
       // .DAT 파일만 필터링
       List<String> files = stringValue.split('\n').where((file) => file.endsWith('.DAT')).toList();
+
       setState(() {
         fileList.addAll(files);
+        isLoading = false;
       });
     } catch (e) {
       print('Error reading SD card file list: $e');
+      setState(() {
+        errorMessage = 'Failed to read SD card file list. Please try again.';
+        isLoading = false;
+      });
     }
   }
 
@@ -42,7 +55,11 @@ class _DataReceivingPageState extends State<DataReceivingPage> {
       appBar: AppBar(
         title: Text('SD Card File List'),
       ),
-      body: ListView.builder(
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : errorMessage != null
+          ? Center(child: Text(errorMessage!, style: TextStyle(color: Colors.red)))
+          : ListView.builder(
         itemCount: fileList.length,
         itemBuilder: (context, index) {
           return ListTile(
